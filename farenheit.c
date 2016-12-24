b4b628356c38ead76e2174b8536b2a65b7d8b680
floydmate <floydmate@gmail.com>
2016-12-24 01:36:06 -0600

    celcius - farenheit table

--- farenheit.c
+++ farenheit.c
@@ -1,14 +1,20 @@
-#import <stdio.h>
+#include <stdio.h>
+/*print Farenheit-Celcius table*/
 
-main(){
- int fahr,celcius;
- int lower,upper,step;
+#define STEP 20
+int main(){
+ float fahr,celcius;
+ int lower,upper;
 
  lower = 0;
  upper = 300;
- step = 20;
 
  fahr = lower;
 
- while(fahr <=
+ while(fahr <=upper){
+    celcius = (5.0/9.0) * (fahr-32.0);
+    printf("%0.3f %6.1f\n",fahr,celcius);
+    fahr = fahr+STEP;
+ }
+ return 0;
 }
